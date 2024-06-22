234e848c48a42ff4590d31ea4115b7f894b16e09
MANSUN KASYAP <78608125+mansunkasyap@users.noreply.github.com>
2024-06-22 11:57:33 +0530

    Update FetchData.js

--- FetchData.js
+++ FetchData.js
@@ -67,3 +67,107 @@ console.log(human.canRun());
 console.log(human.getAge());
 console.log(Human.getHeight());
 // Array.from()
+// Inheritance 
+// 1. Single Inherit
+// 2. Multilevel 
+// 3. Multiple inheritance (NOT POSIBLE)
+// 4. Hierarchical 
+
+
+
+// /// Class in JS
+// class Human{
+//     // console.log(this);
+//     // static h = 90;
+//     age ;
+//     static height = 8.0;
+//     constructor(age, height){
+//         this.age = age 
+//     }
+
+
+//     getAge(){
+//         return this.age;
+//     }
+//     static getHeight(){
+//         console.log("comes Here..........");
+//         return this.height;
+//     }
+
+//     canRun(){
+//         console.log("A Human can run");
+//         return null;
+//     }
+// }
+
+// class Person extends Human{
+//     isMale;
+//     constructor(){
+//         this.isMale = true;
+//     }
+// }
+
+// // const human = new Human(40, 5.5)
+// // console.log(human.canRun());
+// // console.log(human.getAge());
+// // console.log(Human.getHeight());
+// // Array.from() 
+
+// class Mia extends Person
+// {
+//     skinTone ;
+//     rate;
+//     // constructor(){
+//     //     super(12, 20);
+//     //     console.log(this.age);
+//     // }
+//     constructor(skinTone, rate){
+//         super(0, 0);
+//         this.skinTone = skinTone 
+//         this.rate = rate
+//     }
+//     getTone(){
+//         return this.skinTone;
+//     }
+
+//     getRate(){
+//         return this.rate;
+//     }
+// }
+
+// class Johnny extends Person{
+
+// }
+
+// let mia =  new Mia("russian",6000)
+
+// console.log(mia.getTone());
+// console.log(mia.getRate());
+
+// let private = 90;
+// console.log(private);
+
+
+/// Story of this keyword
+
+
+// console.log(this);
+
+// val = 90;
+// x = 900
+
+// function fun(val){ 
+//     console.log(this.val, this.x);
+// }
+
+// fun(12);
+
+let objectOne = {
+    userName : "Mayank",
+    isLogin : true,
+    fun : function (){
+        console.log(this);
+    }
+}
+
+objectOne.fun();
